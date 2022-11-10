428b1d6ad02cf051090f2919802ed3410ae1d418
RafaelPereira86 <carlosrafael@live.com.pt>
2022-11-10 12:38:44 +0000

    initial commit

---
+++ app/src/main/java/com/example/trabalholam/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.trabalholam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email;
+    EditText password;
+    Button btn;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.editTextemail);
+        password = findViewById(R.id.editTextpass);
+        textView = findViewById(R.id.textView2);
+        btn = findViewById(R.id.btnlogin);
+        btn.setOnClickListener(this::login);
+    }
+
+    public void login(View view) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://alunos.upt.pt/~abilioc/dam.php?func=auth&login=";
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: " + response);
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                textView.setText("That didn't work!");
+            }
+        });
+    }
+}

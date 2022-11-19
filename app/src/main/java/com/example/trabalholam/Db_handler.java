951cc5b7f6a582f8917c1cf69c5e79d204322126
RafaelPereira86 <carlosrafael@live.com.pt>
2022-11-19 19:24:33 +0000

    base de dados v1.0

--- app/src/main/java/com/example/trabalholam/Db_handler.java
+++ app/src/main/java/com/example/trabalholam/Db_handler.java
@@ -6,15 +6,46 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class Db_handler extends SQLiteOpenHelper {
-    private static final int DATABASE_VERSION = 1;
-    private static final String DATABASE_NAME = "UTILIZADORES";
-    private static final String TABLE_NAME = "utilizadores";
 
-    private static final String LOGIN = "number";
-    private static final String PASSWORD = "password";
-    private static final String Funcao = "func";
+    Context context;
+    //DEFINICOES BASE DE DADODS
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "mydatabase";
 
-    private Context context;
+
+    //NOME DAS TABELAS
+    public static final String DB_AL_TABLE = "AL";
+    public static final String DB_UC_TABLE = "UC";
+    public static final String DB_AL_UC_NOTAS_TABLE = "NOTA";
+    public static final String DB_AL_UC_HORARIO_TABLE = "HR";
+
+
+    //TABELA ALUNO
+    public static final String ALNUM = "ALNUM";
+    public static final String ALPASSWORD = "ALPASSWORD";
+    public static final String ALTOKEN = "ALTOKEN";
+
+    //TABELA UC
+    public static final String UCCOD = "UCCOD";
+    public static final String UCNOME = "UCNOME";
+
+    //TABELA HORARIO
+    public static final String HORARIOID = "HORARIOID";
+    public static final String AL_HORARIO_NUM = "NUMALUNO";
+    public static final String UC_HORARIO_COD = "UC_HORARIO_COD";
+    public static final String HORARIODIA = "HORARIODIA";
+    public static final String HORIRIOINI = "HORIRIOINI";
+    public static final String HORARIOFIN = "HORARIOFIN";
+    public static final String HORARIOTIPO = "HORARIOTIPO";
+
+    //TABELA NOTA
+    public static final String NOTAID = "NOTA_ID";
+    public static final String AL_NOTA_NUM = "NUMALUNO";
+    public static final String UC_NOTA_COD = "COD_UC";
+    public static final String NOTA = "NOTA";
+
+
+
 
     public Db_handler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -23,13 +54,22 @@ public class Db_handler extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE" + " TABLE_NAME" + "(" + LOGIN + "INTEGER PRIMARY KEY," + PASSWORD + "TEXT" + Funcao + "TEXT" + ")";
-        db.execSQL(CREATE_CONTACTS_TABLE);
+        String queryTabelaAl = String.format("CREATE TABLE %s(%s INTERGER PRIMARY KEY,%s TEXT,%s TEXT)",DB_AL_TABLE,ALNUM,ALPASSWORD,ALTOKEN);
+        String queryTabelaUc = String.format("CREATE TABLE %s(%s INTERGER PRIMARY KEY,%s TEXT)",DB_UC_TABLE,UCCOD,UCNOME);
+        String queryTabelaHorario = String.format("CREATE TABLE %s(%s INTERGER PRIMARY KEY,%s TEXT,%s TEXT,%s TEXT,%s TEXT,%s TEXT,%s TEXT)",DB_AL_UC_HORARIO_TABLE,HORARIOID,AL_HORARIO_NUM,UC_HORARIO_COD,HORARIODIA,HORIRIOINI,HORARIOFIN,HORARIOTIPO);
+        String queryTabelaNota = String.format("CREATE TABLE %s(%s INTERGER PRIMARY KEY,%s TEXT,%s TEXT,%s TEXT)",DB_AL_UC_NOTAS_TABLE,NOTAID,AL_NOTA_NUM,UC_NOTA_COD,NOTA);
+        db.execSQL(queryTabelaAl);
+        db.execSQL(queryTabelaUc);
+        db.execSQL(queryTabelaHorario);
+        db.execSQL(queryTabelaNota);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_AL_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_UC_TABLE);
+        db.execSQL("DROP TABLE IF EXISTS " + DB_AL_UC_HORARIO_TABLE);
+        db.execSQL(String.format("DROP TABLE IF EXISTS %s",DB_AL_UC_NOTAS_TABLE));
         onCreate(db);
     }
 
